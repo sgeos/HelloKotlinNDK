6bfaa0e7986f5770c0eff8d9115ae3340debb896
Brendan A R Sechter <b-sechter@unext.jp>
2017-02-07 11:21:08 +0900

    Optimized imports.

--- app/src/main/kotlin/com/sennue/hellokotlinndk/MainActivity.kt
+++ app/src/main/kotlin/com/sennue/hellokotlinndk/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.sennue.hellokotlinndk
 
 import android.os.Bundle
-import android.widget.TextView
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
